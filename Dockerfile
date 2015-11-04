441c30b3af42a38af097ba1cecf43fe8c28942a5
Jakub Zalas <jakub@zalas.pl>
2015-11-04 22:18:27 +0000

    Install intl and xdebug extensions

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,10 @@ MAINTAINER Jakub Zalas <jakub@zalas.pl>
 ENV PHP_EXTRA_BUILD_DEPS ""
 ENV PHP_EXTRA_INSTALL_CMD ""
 
-RUN buildDeps="zlib1g-dev $PHP_EXTRA_BUILD_DEPS" \
-    && apt-get update && apt-get install --no-install-recommends -y git $buildDeps && rm -r /var/lib/apt/lists/* \
-    && docker-php-ext-install zip \
-    && $PHP_EXTRA_INSTALL_CMD \
+RUN buildDeps="zlib1g-dev libicu-dev $PHP_EXTRA_BUILD_DEPS" \
+    && apt-get update && apt-get install --no-install-recommends -y git libmemcached11 libmemcachedutil2 libicu52 $buildDeps && rm -r /var/lib/apt/lists/* \
+    && docker-php-ext-install intl zip \
+    && bash -c "$PHP_EXTRA_INSTALL_CMD" \
     && apt-get purge -y --auto-remove -o APT::AutoRemove::RecommendsImportant=false -o APT::AutoRemove::SuggestsImportant=false $buildDeps
 
 COPY php.ini $PHP_INI_DIR/
--- update.sh
+++ update.sh
@@ -5,14 +5,17 @@ versions=( "${versions[@]%/}" )
 
 declare PHP_EXTRA_BUILD_DEPS=()
 PHP_EXTRA_BUILD_DEPS["54"]="libmemcached-dev"
-PHP_EXTRA_BUILD_DEPS["55"]="libmemcached-dev"
-PHP_EXTRA_BUILD_DEPS["56"]="libmemcached-dev"
+PHP_EXTRA_BUILD_DEPS["55"]=${PHP_EXTRA_BUILD_DEPS["54"]}
+PHP_EXTRA_BUILD_DEPS["56"]=${PHP_EXTRA_BUILD_DEPS["54"]}
 PHP_EXTRA_BUILD_DEPS["70"]=""
 
 declare PHP_EXTRA_INSTALL_CMD=()
-PHP_EXTRA_INSTALL_CMD["54"]="pecl install memcache && docker-php-ext-enable memcache && pecl install memcached && docker-php-ext-enable memcached"
-PHP_EXTRA_INSTALL_CMD["55"]="pecl install memcache && docker-php-ext-enable memcache && pecl install memcached && docker-php-ext-enable memcached"
-PHP_EXTRA_INSTALL_CMD["56"]="pecl install memcache && docker-php-ext-enable memcache && pecl install memcached && docker-php-ext-enable memcached"
+PHP_EXTRA_INSTALL_CMD["54"]="
+    pecl install memcache && docker-php-ext-enable memcache
+ && pecl install memcached && docker-php-ext-enable memcached
+ && pecl install xdebug && docker-php-ext-enable xdebug"
+PHP_EXTRA_INSTALL_CMD["55"]=${PHP_EXTRA_INSTALL_CMD["54"]}
+PHP_EXTRA_INSTALL_CMD["56"]=${PHP_EXTRA_INSTALL_CMD["54"]}
 PHP_EXTRA_INSTALL_CMD["70"]=""
 
 for version in "${versions[@]}"; do
@@ -21,9 +24,10 @@ for version in "${versions[@]}"; do
     echo "Updating $version"
     var=$(echo $version|sed -e 's/\.//g')
     php_extra_install_cmd=$(echo ${PHP_EXTRA_INSTALL_CMD[$var]} | sed -e 's/[\/&]/\\&/g')
+    php_extra_build_deps=${PHP_EXTRA_BUILD_DEPS[$var]}
     cat Dockerfile \
         | sed -e 's/FROM php:.*/FROM php:'$version'/' \
-              -e 's/ENV PHP_EXTRA_BUILD_DEPS.*/ENV PHP_EXTRA_BUILD_DEPS "'${PHP_EXTRA_BUILD_DEPS[$var]}'"/' \
+              -e "s/ENV PHP_EXTRA_BUILD_DEPS.*/ENV PHP_EXTRA_BUILD_DEPS \"$php_extra_build_deps\"/" \
               -e "s/ENV PHP_EXTRA_INSTALL_CMD.*/ENV PHP_EXTRA_INSTALL_CMD \"$php_extra_install_cmd\"/" > $version/Dockerfile
     cp php.ini $version/
 done
